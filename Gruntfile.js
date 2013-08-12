60153d9ffe468c60b561119d568ed1b95807a515
Takuto Wada <takuto.wada@gmail.com>
2013-08-12 09:43:52 +0900

    Big restructuring: Trying to extract espower module

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,45 +12,9 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: pkg,
-        ver: '<%= pkg.version %>',
-        destDir: 'espowered_tests',
-        clean: {
-            dist: ['<%= destDir %>/']
-        },
-        copy: {
-            no_inst: {
-                files: [
-                    {
-                        expand: true,     // Enable dynamic expansion.
-                        cwd: 'not_instrumented_tests/',      // Src matches are relative to this path.
-                        src: ['**/*.js'], // Actual pattern(s) to match.
-                        dest: '<%= destDir %>/',   // Destination path prefix.
-                        ext: '.js'   // Dest filepaths will have this extension.
-                    },
-                ]
-            }
-        },
-        shell: {
-            mkdir: {
-                command: 'mkdir <%= destDir %>'
-            }
-        },
-        espower: {
-            powerAssertVariableName: 'assert',
-            dest: '<%= destDir %>',
-            dir: ['mocha_tests/*']
-        },
         unit: {
             dir: ['test/**/*.js']
         },
-        mochaTest: {
-            espowered: {
-                options: {
-                    reporter: 'dot'
-                },
-                src: ['<%= destDir %>/**/*.js']
-            }
-        }
     });
 
 
@@ -110,42 +74,5 @@ module.exports = function(grunt) {
     });
 
 
-    grunt.registerTask('espower', function() {
-        var done = this.async();
-        var espower = require('./lib/espower'),
-            esprima = require('esprima'),
-            escodegen = require('escodegen'),
-            fs = require('fs'),
-            path = require('path');
-
-        grunt.config.get('espower.dir').forEach(function (pattern) {
-            grunt.log.write('searching for ' + pattern + '...').ok();
-            grunt.file.glob(pattern, function (err, files) {
-                if (err) {
-                    grunt.log.write('error ' + err.message).ok();
-                    return;
-                }
-                files.forEach(function (file) {
-                    grunt.log.write('espower ' + file + '...').ok();
-                    var source = fs.readFileSync(file, 'utf-8'),
-                        tree = esprima.parse(source, {tolerant: true, loc: true, range: true}),
-                        options = {
-                            powerAssertVariableName: grunt.config.get('espower.powerAssertVariableName'),
-                            path: fs.realpathSync(file),
-                            source: source
-                        },
-                        modified = espower(tree, options);
-			        grunt.file.write(path.join(grunt.config.get('espower.dest'), path.basename(file)), escodegen.generate(modified));
-                });
-                done(files.length);
-            });
-        });
-    });
-
-
-    grunt.registerTask('default', ['clean:dist', 'shell:mkdir', 'qunit-test', 'copy:no_inst', 'espower', 'mochaTest:espowered']);
-
     grunt.registerTask('unit', ['qunit-test']);
-
-    grunt.registerTask('functional', ['clean:dist', 'shell:mkdir', 'copy:no_inst', 'espower', 'mochaTest:espowered']);
 };
--- scripts/example_runners/mocha_node_js.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-./bin/espower sandbox/mocha_node.js > sandbox/mocha_node_espowered.js && ./node_modules/.bin/mocha --reporter tap sandbox/mocha_node_espowered.js
-exit 0
