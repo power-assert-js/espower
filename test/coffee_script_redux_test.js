b7abfeeac5b8fb11fa17be94a34f9c446779b8b4
Takuto Wada <takuto.wada@gmail.com>
2013-09-06 14:35:59 +0900

    Rewrite CoffeeScriptRedux spike test.

--- test/coffee_script_redux_test.js
+++ test/coffee_script_redux_test.js
@@ -5,19 +5,11 @@ var espower = require('../lib/espower'),
 describe('CoffeeScriptRedux learning & spike', function () {
 
     it('with CoffeeScriptRedux toolchain', function () {
-        var csCode = 'assert.ok dog.speak() == says';
-
-        var parseOptions = {raw: true};
-        var csAST = CoffeeScript.parse(csCode, parseOptions);
-
-        var compileOptions = {bare: false};
-        var jsAST = CoffeeScript.compile(csAST, compileOptions);
-
-        var espoweredAst = espower(jsAST, {destructive: false, source: csCode, path: '/path/to/foo_test.coffee', powerAssertVariableName: 'assert'});
-
-        var jsGenerateOptions = {compact: true};
-        var jsCode = CoffeeScript.js(espoweredAst, jsGenerateOptions);
-
-        assert.equal(jsCode, "assert.ok(assert._expr(assert._capt(assert._capt(assert._capt(dog,'ident',{start:{line:1,column:10}}).speak(),'funcall',{start:{line:1,column:14}})===assert._capt(says,'ident',{start:{line:1,column:25}}),'binary',{start:{line:1,column:22}}),{start:{line:1,column:10},path:'/path/to/foo_test.coffee'},'assert.ok dog.speak() == says'))");
+        var csCode = 'assert.ok dog.age is three',
+            csAST = CoffeeScript.parse(csCode, {raw: true}),
+            jsAST = CoffeeScript.compile(csAST, {bare: false}),
+            espoweredAst = espower(jsAST, {destructive: false, source: csCode, path: '/path/to/foo_test.coffee', powerAssertVariableName: 'assert'}),
+            jsCode = CoffeeScript.js(espoweredAst, {compact: true});
+        assert.equal(jsCode, "assert.ok(assert._expr(assert._capt(assert._capt(assert._capt(dog,'ident',{start:{line:1,column:10}}).age,'ident',{start:{line:1,column:14}})===assert._capt(three,'ident',{start:{line:1,column:21}}),'binary',{start:{line:1,column:18}}),{start:{line:1,column:10},path:'/path/to/foo_test.coffee'},'assert.ok dog.age is three'))");
     });
 });
