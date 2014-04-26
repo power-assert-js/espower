68d3f1df13f3228bfb8ac6bb7f4353327d1d8e08
Takuto Wada <takuto.wada@gmail.com>
2014-04-26 17:03:42 +0900

    Embedded AST is not required for now.

--- test/espower_option_test.js
+++ test/espower_option_test.js
@@ -85,11 +85,11 @@ describe('instrumentation tests for options', function () {
     describe('source option and path option.', function () {
         it('path: null', function () {
             var instrumentedCode = instrument('assert(falsyStr);', {source: 'assert(falsyStr);'});
-            assert.equal(instrumentedCode, "assert(assert._expr(assert._capt(falsyStr,''),{tree:{'type':'Identifier','name':'falsyStr','loc':{'start':{'line':1,'column':7},'end':{'line':1,'column':15}}},tokens:[{'type':'Identifier','value':'falsyStr','loc':{'start':{'line':1,'column':7},'end':{'line':1,'column':15}}}],content:'assert(falsyStr)'}));");
+            assert.equal(instrumentedCode, "assert(assert._expr(assert._capt(falsyStr,''),{content:'assert(falsyStr)'}));");
         });
         it('with source and path', function () {
             var instrumentedCode = instrument('assert(falsyStr);', {source: 'assert(falsyStr);', path: '/path/to/baz_test.js'});
-            assert.equal(instrumentedCode, "assert(assert._expr(assert._capt(falsyStr,''),{tree:{'type':'Identifier','name':'falsyStr','loc':{'start':{'line':1,'column':7},'end':{'line':1,'column':15}}},tokens:[{'type':'Identifier','value':'falsyStr','loc':{'start':{'line':1,'column':7},'end':{'line':1,'column':15}}}],content:'assert(falsyStr)',filepath:'/path/to/baz_test.js'}));");
+            assert.equal(instrumentedCode, "assert(assert._expr(assert._capt(falsyStr,''),{content:'assert(falsyStr)',filepath:'/path/to/baz_test.js'}));");
         });
     });
 
@@ -97,11 +97,11 @@ describe('instrumentation tests for options', function () {
     describe('powerAssertVariableName option.', function () {
         it('default is "assert"', function () {
             var instrumentedCode = instrument('assert(falsyStr);', {source: 'assert(falsyStr);'});
-            assert.equal(instrumentedCode, "assert(assert._expr(assert._capt(falsyStr,''),{tree:{'type':'Identifier','name':'falsyStr','loc':{'start':{'line':1,'column':7},'end':{'line':1,'column':15}}},tokens:[{'type':'Identifier','value':'falsyStr','loc':{'start':{'line':1,'column':7},'end':{'line':1,'column':15}}}],content:'assert(falsyStr)'}));");
+            assert.equal(instrumentedCode, "assert(assert._expr(assert._capt(falsyStr,''),{content:'assert(falsyStr)'}));");
         });
         it('powerAssertVariableName: "test"', function () {
             var instrumentedCode = instrument('test.ok(falsyStr);', {source: 'test.ok(falsyStr);', powerAssertVariableName: 'test'});
-            assert.equal(instrumentedCode, "test.ok(test._expr(test._capt(falsyStr,''),{tree:{'type':'Identifier','name':'falsyStr','loc':{'start':{'line':1,'column':8},'end':{'line':1,'column':16}}},tokens:[{'type':'Identifier','value':'falsyStr','loc':{'start':{'line':1,'column':8},'end':{'line':1,'column':16}}}],content:'test.ok(falsyStr)'}));");
+            assert.equal(instrumentedCode, "test.ok(test._expr(test._capt(falsyStr,''),{content:'test.ok(falsyStr)'}));");
         });
         it('not instrumented if powerAssertVariableName and actual variable name is different.', function () {
             var instrumentedCode = instrument('assert.ok(falsyStr);', {source: 'assert.ok(falsyStr);', powerAssertVariableName: 'test'});
@@ -111,9 +111,7 @@ describe('instrumentation tests for options', function () {
 
 
     describe('lineSeparator option', function () {
-        var lineDetected = "var falsyStr='';assert.ok(assert._expr(assert._capt(falsyStr,''),{tree:{'type':'Identifier','name':'falsyStr','loc':{'start':{'line':3,'column':10},'end':{'line':3,'column':18}}},tokens:[{'type':'Identifier','value':'falsyStr','loc':{'start':{'line':3,'column':10},'end':{'line':3,'column':18}}}],content:'assert.ok(falsyStr)'}));",
-
-            lineNotDetected = "var falsyStr='';assert.ok(assert._expr(assert._capt(falsyStr,''),{tree:{'type':'Identifier','name':'falsyStr','loc':{'start':{'line':3,'column':10},'end':{'line':3,'column':18}}},tokens:[{'type':'Identifier','value':'falsyStr','loc':{'start':{'line':3,'column':10},'end':{'line':3,'column':18}}}]}));";
+        var lineDetected = "var falsyStr='';assert.ok(assert._expr(assert._capt(falsyStr,''),{content:'assert.ok(falsyStr)'}));";
 
         function lineSeparatorTest (name, lineSeparatorInCode, options, expected) {
             it(name, function () {
