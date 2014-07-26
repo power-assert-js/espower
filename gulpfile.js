164c578f05deb632ef1b0007d2460b8371e1b9d8
Takuto Wada <takuto.wada@gmail.com>
2014-07-26 13:42:03 +0900

    Handle stream with bufferstreams, not streamify.

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,8 @@ var gulp = require('gulp'),
     source = require('vinyl-source-stream'),
     through = require('through2'),
     browserify = require('browserify'),
-    streamify = require('gulp-streamify'),
     derequire = require('derequire'),
+    BufferStreams = require('bufferstreams'),
     config = {
         bundle: {
             standalone: 'espower',
@@ -104,11 +104,18 @@ gulp.task('clean_coverage', function (done) {
     del([config.coverage.filename], done);
 });
 
-var dereqBundle = through.obj(function (file, encoding, callback) {
+var dereq = through.obj(function (file, encoding, callback) {
     if (file.isNull()) {
         this.push(file);
     } else if (file.isBuffer()) {
         file.contents = new Buffer(derequire(file.contents.toString('utf8')));
+        this.push(file);
+    } else if (file.isStream()) {
+        file.contents = file.contents.pipe(new BufferStreams(function(err, buf, cb) {
+            if(err) throw err;
+            buf = new Buffer(derequire(buf.toString('utf8')));
+            cb(null, buf);
+        }));
         this.push(file);
     }
     callback();
@@ -118,7 +125,7 @@ gulp.task('bundle', ['clean_bundle'], function() {
     var bundleStream = browserify({entries: config.bundle.srcFile, standalone: config.bundle.standalone}).bundle();
     return bundleStream
         .pipe(source(config.bundle.destName))
-        .pipe(streamify(dereqBundle))
+        .pipe(dereq)
         .pipe(gulp.dest(config.bundle.destDir));
 });
 
