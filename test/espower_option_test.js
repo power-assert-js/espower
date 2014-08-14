6dbfe726a3f8328b4b9649be17abad4707859770
Takuto Wada <takuto.wada@gmail.com>
2014-08-14 17:39:31 +0900

    test(espower): add missing test for options.patterns

--- test/espower_option_test.js
+++ test/espower_option_test.js
@@ -36,6 +36,18 @@ describe('espower.defaultOptions()', function () {
     it('destructive: false', function () {
         assert.equal(this.options.destructive, false);
     });
+    it('patterns: Array', function () {
+        assert.deepEqual(this.options.patterns, [
+            'assert(value, [message])',
+            'assert.ok(value, [message])',
+            'assert.equal(actual, expected, [message])',
+            'assert.notEqual(actual, expected, [message])',
+            'assert.strictEqual(actual, expected, [message])',
+            'assert.notStrictEqual(actual, expected, [message])',
+            'assert.deepEqual(actual, expected, [message])',
+            'assert.notDeepEqual(actual, expected, [message])'
+        ]);
+    });
 });
 
 
