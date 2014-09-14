57cd8a9745e45ffee6b49265792d266dfff8cd25
Takuto Wada <takuto.wada@gmail.com>
2014-09-15 06:23:08 +0900

    test(espower): add bug reproduction case
    when FunctionExpression begins right after the assertion, its params will be transformed accidentally.

--- test/instrumentation_test.js
+++ test/instrumentation_test.js
@@ -333,6 +333,8 @@ describe('instrumentation spec', function () {
 
 
     describe('FunctionExpression will not be instrumented', function () {
+        inst("assert(function (a, b) { return a + b; });",
+             "assert(function(a,b){return a+b;});");
         inst("assert(baz === (function (a, b) { return a + b; })(foo, bar));",
              "assert(assert._expr(assert._capt(assert._capt(baz,'arguments/0/left')===assert._capt(function(a,b){return a+b;}(assert._capt(foo,'arguments/0/right/arguments/0'),assert._capt(bar,'arguments/0/right/arguments/1')),'arguments/0/right'),'arguments/0'),{content:'assert(baz === function (a, b) {return a + b;}(foo, bar))',filepath:'/path/to/some_test.js',line:1}));");
     });
