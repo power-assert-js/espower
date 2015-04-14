17d7d6c03991e91f48f2fcf3fc2060bef6a570be
Takuto Wada <takuto.wada@gmail.com>
2015-04-14 20:48:26 +0900

    test(espower): more tests on SpreadElement

--- test/instrumentation_test.js
+++ test/instrumentation_test.js
@@ -391,8 +391,10 @@ describe('instrumentation spec', function () {
         describe('SpreadElement', function () {
             inst("assert(hello(...names));",
                  "assert(assert._expr(assert._capt(hello(...assert._capt(names,'arguments/0/arguments/0/argument')),'arguments/0'),{content:'assert(hello(...names))',filepath:'/path/to/some_test.js',line:1}));");
-            inst("assert(f(head, ...iter, ...tail));",
-                 "assert(assert._expr(assert._capt(f(assert._capt(head,'arguments/0/arguments/0'),...assert._capt(iter,'arguments/0/arguments/1/argument'),...assert._capt(tail,'arguments/0/arguments/2/argument')),'arguments/0'),{content:'assert(f(head, ...iter, ...tail))',filepath:'/path/to/some_test.js',line:1}));");
+            inst("assert([head, ...tail].length);",
+                 "assert(assert._expr(assert._capt([assert._capt(head,'arguments/0/object/elements/0'),...assert._capt(tail,'arguments/0/object/elements/1/argument')].length,'arguments/0'),{content:'assert([head,...tail].length)',filepath:'/path/to/some_test.js',line:1}));");
+            inst("assert(f(head, ...iter(), ...[foo, bar]));",
+                 "assert(assert._expr(assert._capt(f(assert._capt(head,'arguments/0/arguments/0'),...assert._capt(iter(),'arguments/0/arguments/1/argument'),...[assert._capt(foo,'arguments/0/arguments/2/argument/elements/0'),assert._capt(bar,'arguments/0/arguments/2/argument/elements/1')]),'arguments/0'),{content:'assert(f(head, ...iter(), ...[foo,bar]))',filepath:'/path/to/some_test.js',line:1}));");
         });
 
     });
