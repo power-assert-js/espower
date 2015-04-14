32502c3388508ab51fa3431f0b269943843c9d92
Takuto Wada <takuto.wada@gmail.com>
2015-04-14 18:51:48 +0900

    chore(espower): update script for debugging

--- esp.js
+++ esp.js
@@ -1,9 +1,10 @@
 var espower = require('./index'),
-    esprima = require('esprima'),
+    acorn = require('acorn'),
     escodegen = require('escodegen');
 
 var jsCode = process.argv[2];
-var jsAst = esprima.parse(jsCode, {tolerant: true, loc: true, tokens: true});
+var parserOptions = {ecmaVersion: 6, locations: true};
+var jsAst = acorn.parse(jsCode, parserOptions);
 var modifiedAst = espower(jsAst);
 
 console.log(escodegen.generate(modifiedAst));
