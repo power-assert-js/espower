d0fe35169da98b447ace9e969b8d395d41e3c6df
Takuto Wada <takuto.wada@gmail.com>
2016-02-02 01:23:17 +0900

    feat(espower): embed recorder class at top level

--- test/fixtures/Identifier/expected.js
+++ test/fixtures/Identifier/expected.js
@@ -1,15 +1,37 @@
-var _rec12 = new _powerAssertRecorder();
-var _rec11 = new _powerAssertRecorder();
-var _rec10 = new _powerAssertRecorder();
-var _rec9 = new _powerAssertRecorder();
-var _rec8 = new _powerAssertRecorder();
-var _rec7 = new _powerAssertRecorder();
-var _rec6 = new _powerAssertRecorder();
-var _rec5 = new _powerAssertRecorder();
-var _rec4 = new _powerAssertRecorder();
-var _rec3 = new _powerAssertRecorder();
-var _rec2 = new _powerAssertRecorder();
-var _rec1 = new _powerAssertRecorder();
+var _rec12 = new _PowerAssertRecorder1();
+var _rec11 = new _PowerAssertRecorder1();
+var _rec10 = new _PowerAssertRecorder1();
+var _rec9 = new _PowerAssertRecorder1();
+var _rec8 = new _PowerAssertRecorder1();
+var _rec7 = new _PowerAssertRecorder1();
+var _rec6 = new _PowerAssertRecorder1();
+var _rec5 = new _PowerAssertRecorder1();
+var _rec4 = new _PowerAssertRecorder1();
+var _rec3 = new _PowerAssertRecorder1();
+var _rec2 = new _PowerAssertRecorder1();
+var _rec1 = new _PowerAssertRecorder1();
+var _PowerAssertRecorder1 = function () {
+    function PowerAssertRecorder() {
+        this.captured = [];
+    }
+    PowerAssertRecorder.prototype._capt = function _capt(value, espath) {
+        this.captured.push({
+            value: value,
+            espath: espath
+        });
+        return value;
+    };
+    PowerAssertRecorder.prototype._expr = function _expr(value, source) {
+        return {
+            powerAssertContext: {
+                value: value,
+                events: this.captured
+            },
+            source: source
+        };
+    };
+    return PowerAssertRecorder;
+}();
 'use strict';
 assert(_rec1._expr(_rec1._capt(falsyStr, 'arguments/0'), {
     content: 'assert(falsyStr)',
