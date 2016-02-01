fa304ebffa264b5dce0b8068e7133d1513864c9b
Takuto Wada <takuto.wada@gmail.com>
2016-02-02 01:30:38 +0900

    chore(espower): unshift node insertion callback

--- test/fixtures/Identifier/expected.js
+++ test/fixtures/Identifier/expected.js
@@ -1,15 +1,3 @@
-var _rec12 = new _PowerAssertRecorder1();
-var _rec11 = new _PowerAssertRecorder1();
-var _rec10 = new _PowerAssertRecorder1();
-var _rec9 = new _PowerAssertRecorder1();
-var _rec8 = new _PowerAssertRecorder1();
-var _rec7 = new _PowerAssertRecorder1();
-var _rec6 = new _PowerAssertRecorder1();
-var _rec5 = new _PowerAssertRecorder1();
-var _rec4 = new _PowerAssertRecorder1();
-var _rec3 = new _PowerAssertRecorder1();
-var _rec2 = new _PowerAssertRecorder1();
-var _rec1 = new _PowerAssertRecorder1();
 var _PowerAssertRecorder1 = function () {
     function PowerAssertRecorder() {
         this.captured = [];
@@ -32,6 +20,18 @@ var _PowerAssertRecorder1 = function () {
     };
     return PowerAssertRecorder;
 }();
+var _rec1 = new _PowerAssertRecorder1();
+var _rec2 = new _PowerAssertRecorder1();
+var _rec3 = new _PowerAssertRecorder1();
+var _rec4 = new _PowerAssertRecorder1();
+var _rec5 = new _PowerAssertRecorder1();
+var _rec6 = new _PowerAssertRecorder1();
+var _rec7 = new _PowerAssertRecorder1();
+var _rec8 = new _PowerAssertRecorder1();
+var _rec9 = new _PowerAssertRecorder1();
+var _rec10 = new _PowerAssertRecorder1();
+var _rec11 = new _PowerAssertRecorder1();
+var _rec12 = new _PowerAssertRecorder1();
 'use strict';
 assert(_rec1._expr(_rec1._capt(falsyStr, 'arguments/0'), {
     content: 'assert(falsyStr)',
