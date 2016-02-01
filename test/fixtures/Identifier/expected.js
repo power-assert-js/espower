5fa4e96c428ea4c7b92c01b75f3ee3f85ca0dc21
Takuto Wada <takuto.wada@gmail.com>
2016-02-01 17:30:03 +0900

    feat(espower): use generated identifier for per-argument value recorder

--- test/fixtures/Identifier/expected.js
+++ test/fixtures/Identifier/expected.js
@@ -11,57 +11,57 @@ var _rec = new _powerAssertRecorder();
 var _rec = new _powerAssertRecorder();
 var _rec = new _powerAssertRecorder();
 'use strict';
-assert(assert._expr(assert._capt(falsyStr, 'arguments/0'), {
+assert(_rec._expr(_rec._capt(falsyStr, 'arguments/0'), {
     content: 'assert(falsyStr)',
     filepath: 'path/to/some_test.js',
     line: 3
 }));
-assert(assert._expr(assert._capt(falsyStr, 'arguments/0'), {
+assert(_rec._expr(_rec._capt(falsyStr, 'arguments/0'), {
     content: 'assert(falsyStr, messageStr)',
     filepath: 'path/to/some_test.js',
     line: 5
 }), messageStr);
-assert.equal(assert._expr(assert._capt(str, 'arguments/0'), {
+assert.equal(_rec._expr(_rec._capt(str, 'arguments/0'), {
     content: 'assert.equal(str, anotherStr)',
     filepath: 'path/to/some_test.js',
     line: 7
-}), assert._expr(assert._capt(anotherStr, 'arguments/1'), {
+}), _rec._expr(_rec._capt(anotherStr, 'arguments/1'), {
     content: 'assert.equal(str, anotherStr)',
     filepath: 'path/to/some_test.js',
     line: 7
 }));
-assert.equal(assert._expr(assert._capt(str, 'arguments/0'), {
+assert.equal(_rec._expr(_rec._capt(str, 'arguments/0'), {
     content: 'assert.equal(str, anotherStr)',
     filepath: 'path/to/some_test.js',
     line: 9
-}), assert._expr(assert._capt(anotherStr, 'arguments/1'), {
+}), _rec._expr(_rec._capt(anotherStr, 'arguments/1'), {
     content: 'assert.equal(str, anotherStr)',
     filepath: 'path/to/some_test.js',
     line: 9
 }));
-assert.equal(assert._expr(assert._capt(str, 'arguments/0'), {
+assert.equal(_rec._expr(_rec._capt(str, 'arguments/0'), {
     content: 'assert.equal(str, anotherStr, messageStr)',
     filepath: 'path/to/some_test.js',
     line: 14
-}), assert._expr(assert._capt(anotherStr, 'arguments/1'), {
+}), _rec._expr(_rec._capt(anotherStr, 'arguments/1'), {
     content: 'assert.equal(str, anotherStr, messageStr)',
     filepath: 'path/to/some_test.js',
     line: 14
 }), messageStr);
-assert.equal(assert._expr(assert._capt(str, 'arguments/0'), {
+assert.equal(_rec._expr(_rec._capt(str, 'arguments/0'), {
     content: 'assert.equal(str, anotherStr)',
     filepath: 'path/to/some_test.js',
     line: 16
-}), assert._expr(assert._capt(anotherStr, 'arguments/1'), {
+}), _rec._expr(_rec._capt(anotherStr, 'arguments/1'), {
     content: 'assert.equal(str, anotherStr)',
     filepath: 'path/to/some_test.js',
     line: 16
 }));
-assert.equal(assert._expr(assert._capt(str, 'arguments/0'), {
+assert.equal(_rec._expr(_rec._capt(str, 'arguments/0'), {
     content: 'assert.equal(str, yetAnotherStr)',
     filepath: 'path/to/some_test.js',
     line: 21
-}), assert._expr(assert._capt(yetAnotherStr, 'arguments/1'), {
+}), _rec._expr(_rec._capt(yetAnotherStr, 'arguments/1'), {
     content: 'assert.equal(str, yetAnotherStr)',
     filepath: 'path/to/some_test.js',
     line: 21
--- test/fixtures/Mocha/expected.js
+++ test/fixtures/Mocha/expected.js
@@ -11,7 +11,7 @@ describe('Array#indexOf()', function () {
     it('should return index when the value is present', function () {
         var _rec = new _powerAssertRecorder();
         var who = 'ariya', two = 2;
-        assert(assert._expr(assert._capt(assert._capt(assert._capt(this.ary, 'arguments/0/left/callee/object').indexOf(assert._capt(who, 'arguments/0/left/arguments/0')), 'arguments/0/left') === assert._capt(two, 'arguments/0/right'), 'arguments/0'), {
+        assert(_rec._expr(_rec._capt(_rec._capt(_rec._capt(this.ary, 'arguments/0/left/callee/object').indexOf(_rec._capt(who, 'arguments/0/left/arguments/0')), 'arguments/0/left') === _rec._capt(two, 'arguments/0/right'), 'arguments/0'), {
             content: 'assert(this.ary.indexOf(who) === two)',
             filepath: 'path/to/some_test.js',
             line: 11
@@ -20,7 +20,7 @@ describe('Array#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
         var _rec = new _powerAssertRecorder();
         var minusOne = -1, two = 2;
-        assert.ok(assert._expr(assert._capt(assert._capt(assert._capt(this.ary, 'arguments/0/left/callee/object').indexOf(assert._capt(two, 'arguments/0/left/arguments/0')), 'arguments/0/left') === assert._capt(minusOne, 'arguments/0/right'), 'arguments/0'), {
+        assert.ok(_rec._expr(_rec._capt(_rec._capt(_rec._capt(this.ary, 'arguments/0/left/callee/object').indexOf(_rec._capt(two, 'arguments/0/left/arguments/0')), 'arguments/0/left') === _rec._capt(minusOne, 'arguments/0/right'), 'arguments/0'), {
             content: 'assert.ok(this.ary.indexOf(two) === minusOne, \'THIS IS AN ASSERTION MESSAGE\')',
             filepath: 'path/to/some_test.js',
             line: 15
