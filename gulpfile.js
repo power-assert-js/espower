d67cc5fffb3a32884604c933212922775a00c9ca
Takuto Wada <takuto.wada@gmail.com>
2019-01-10 18:59:28 +0900

    refactor: replace gulp test with npm script

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
-var mocha = require('gulp-mocha');
 var del = require('del');
 var path = require('path');
 var fs = require('fs');
@@ -19,34 +18,6 @@ var config = {
     }
 };
 
-
-function runMochaSimply() {
-    return gulp
-        .src(config.test.base + config.test.pattern, {read: false})
-        .pipe(mocha({
-            ui: 'bdd',
-            reporter: 'dot'
-        }))
-        .on('error', gutil.log);
-}
-
-gulp.task('watch', function () {
-    gulp.watch('{lib,test}/**/*.js', runMochaSimply);
-    runMochaSimply();
-});
-
-gulp.task('generate_recorder_json', function (done) {
-    var filepath = path.join(__dirname, 'power-assert-recorder.js');
-    var ast = acorn.parse(fs.readFileSync(filepath), { ecmaVersion: 6, locations: true });
-    var callexp = espurify(ast).body[0].expression.right;
-    fs.writeFileSync(path.join(__dirname, 'lib', 'power-assert-recorder.json'), JSON.stringify(callexp, null, 2));
-    done();
-});
-
-gulp.task('unit', ['generate_recorder_json'], function () {
-    return runMochaSimply();
-});
-
 gulp.task('clean_coverage', function () {
     del.sync([config.coverage.filename]);
 });
@@ -58,5 +29,3 @@ gulp.task('lint', function() {
 });
 
 gulp.task('clean', ['clean_coverage']);
-
-gulp.task('test', ['unit']);
