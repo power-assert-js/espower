9150eca00fe2748f24e9764581452ccd9ee0db14
Takuto Wada <takuto.wada@gmail.com>
2019-01-10 18:49:40 +0900

    refactor: replace gulp coverage with npm script

--- gulpfile.js
+++ gulpfile.js
@@ -13,61 +13,12 @@ var config = {
     jshint: {
         src: './lib/**/*.js'
     },
-    coverage: {
-        filename: 'coverage.lcov'
-    },
     test: {
         base: './test/',
         pattern: '**/*_test.js'
     }
 };
 
-
-function captureStdout (filespec) {
-    var orig, log = '';
-    function spy (str) {
-        log += str;
-    }
-    function pass (file, encoding, callback) {
-        this.push(file);
-        callback();
-    }
-    return {
-        start: through.obj(pass, function (callback) {
-            orig = process.stdout.write;
-            process.stdout.write = spy;
-            callback();
-        }),
-        finish: through.obj(pass, function (callback) {
-            var file = new gutil.File(filespec);
-            file.contents = new Buffer(log);
-            this.push(file);
-            process.stdout.write = orig;
-            log = '';
-            orig = null;
-            callback();
-        })
-    };
-}
-
-function runMochaWithBlanket() {
-    var blanket = require('./coverage/blanket');
-    var capt = captureStdout({
-        cwd: __dirname,
-        base: __dirname,
-        path: __dirname + '/' + config.coverage.filename
-    });
-    return gulp
-        .src(config.test.base + config.test.pattern, {read: false})
-        .pipe(capt.start)
-        .pipe(mocha({
-            ui: 'bdd',
-            reporter: 'mocha-lcov-reporter'
-        }))
-        .pipe(capt.finish)
-        .pipe(gulp.dest('.'))
-        .on('error', gutil.log);
-}
 
 function runMochaSimply() {
     return gulp
@@ -98,10 +49,6 @@ gulp.task('unit', ['generate_recorder_json'], function () {
 
 gulp.task('clean_coverage', function () {
     del.sync([config.coverage.filename]);
-});
-
-gulp.task('coverage', ['clean_coverage'], function () {
-    return runMochaWithBlanket();
 });
 
 gulp.task('lint', function() {
