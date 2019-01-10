433284600a2d0abb49e3786ea13b96dab92f057a
Takuto Wada <takuto.wada@gmail.com>
2019-01-10 23:29:26 +0900

    refactor: replace gulp lint with npm script

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,1 @@
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var stylish = require('jshint-stylish');
-var config = {
-    jshint: {
-        src: './lib/**/*.js'
-    }
-};
-
-gulp.task('lint', function() {
-    return gulp.src(config.jshint.src)
-        .pipe(jshint())
-        .pipe(jshint.reporter(stylish));
-});
