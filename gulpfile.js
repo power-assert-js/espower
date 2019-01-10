dc9bf5328a7500c609e9a7c1a844d720f0ba3c87
Takuto Wada <takuto.wada@gmail.com>
2019-01-10 23:19:46 +0900

    refactor: replace gulp clean with npm script

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,14 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
-var del = require('del');
-var path = require('path');
-var fs = require('fs');
-var through = require('through2');
-var acorn = require('acorn');
-var espurify = require('espurify');
 var config = {
     jshint: {
         src: './lib/**/*.js'
-    },
-    test: {
-        base: './test/',
-        pattern: '**/*_test.js'
     }
 };
-
-gulp.task('clean_coverage', function () {
-    del.sync([config.coverage.filename]);
-});
 
 gulp.task('lint', function() {
     return gulp.src(config.jshint.src)
         .pipe(jshint())
         .pipe(jshint.reporter(stylish));
 });
-
-gulp.task('clean', ['clean_coverage']);
