89fae53ba22059fe7cfd92a5d2ab3219342cd958
Takuto Wada <takuto.wada@gmail.com>
2019-02-20 23:21:43 +0900

    feat: implement assertion metadata

--- test/metadata_test.js
+++ test/metadata_test.js
@@ -21,22 +21,32 @@ describe.only('simple Identifier: assert(falsy)', () => {
 const falsy = false;
 assert(falsy);
 `;
-  it ('metadata functoin', () => {
+
+  const metadataGenerator = [
+    "var _pwmeta1=(ptnidx,content,filepath,line,extra)=>{",
+    "const version=2,patterns=[",
+    "{",
+    "pattern:'assert(value, [message])',",
+    "args:[",
+    "{index:0,name:'value',kind:'mandatory'},",
+    "{index:1,name:'message',kind:'optional',message:true}",
+    "]",
+    "}",
+    "];",
+    "return Object.assign({version,content,filepath,line},extra,patterns[ptnidx]);",
+    "};"
+  ].join('');
+
+  const metadata = "var _am1=_pwmeta1(0,'assert(falsy)','path/to/some_test.js',3);";
+
+  it ('metadata generator function', () => {
     const { lastLine } = transpile(input, { patterns: ['assert(value, [message])'] });
-    const expected = [
-      "var _pwmeta1=(ptnidx,content,filepath,line,extra)=>{",
-      "const version=2,patterns=[",
-      "{",
-      "pattern:'assert(value, [message])',",
-      "args:[",
-      "{index:0,name:'value',kind:'mandatory'},",
-      "{index:1,name:'message',kind:'optional',message:true}",
-      "]",
-      "}",
-      "];",
-      "return Object.assign({version,content,filepath,line},extra,patterns[ptnidx]);",
-      "};"
-    ].join('');
-    assert.strictEqual(lastLine.substring(0, expected.length), expected);
+    assert.strictEqual(lastLine.substring(0, metadataGenerator.length), metadataGenerator);
+  });
+
+  it('assertion metadata', () => {
+    const { lastLine } = transpile(input, { patterns: ['assert(value, [message])'] });
+    const startAt = metadataGenerator.length;
+    assert.strictEqual(lastLine.substring(startAt, startAt + metadata.length), metadata);
   });
 });
